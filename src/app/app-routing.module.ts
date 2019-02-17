bffff2a2c8ead48f9d5551170f90e66cbb2be1db
Oskar Dadan <skrzynkanof@gmail.com>
2019-02-17 18:08:14 +0100

    poll description page filled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { QuestionnairePageComponent } from './pages/questionnaire-page/questionnaire-page.component'
+import { PollDescriptionPageComponent } from './pages/poll-description-page/poll-description-page.component';
 
 const routes: Routes = [
   { path: '', component: WelcomePageComponent },
-  { path: 'questionnaire', component: QuestionnairePageComponent }
+  { path: 'questionnaire', component: QuestionnairePageComponent },
+  { path: 'poll-description', component: PollDescriptionPageComponent }
 ];
 
 @NgModule({
