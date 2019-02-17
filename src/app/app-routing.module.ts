57e7e5a7b1dab315b00ce1c8087bbb08cb73108b
Oskar Dadan <skrzynkanof@gmail.com>
2019-02-17 19:19:11 +0100

    terms pages filled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { QuestionnairePageComponent } from './pages/questionnaire-page/questionn
 import { PollDescriptionPageComponent } from './pages/poll-description-page/poll-description-page.component';
 import { TermsSoundsPageComponent } from './pages/terms-sounds-page/terms-sounds-page.component';
 import { TermsFrontScenePageComponent } from './pages/terms-front-scene-page/terms-front-scene-page.component';
+import { TermsBackScenePageComponent } from './pages/terms-back-scene-page/terms-back-scene-page.component';
+import { TermsAllAroundScenePageComponent } from './pages/terms-all-around-scene-page/terms-all-around-scene-page.component';
 
 const routes: Routes = [
   { path: '', component: WelcomePageComponent },
   { path: 'questionnaire', component: QuestionnairePageComponent },
   { path: 'poll-description', component: PollDescriptionPageComponent },
   { path: 'terms-sounds', component: TermsSoundsPageComponent },
-  { path: 'terms-front-scene', component: TermsFrontScenePageComponent }
+  { path: 'terms-front-scene', component: TermsFrontScenePageComponent },
+  { path: 'terms-back-scene', component: TermsBackScenePageComponent },
+  { path: 'terms-all-around-scene', component: TermsAllAroundScenePageComponent }
 ];
 
 @NgModule({
