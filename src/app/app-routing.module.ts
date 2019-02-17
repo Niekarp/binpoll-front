998e9af570f08ae2c3428a7387a934a4acb10d99
Oskar Dadan <skrzynkanof@gmail.com>
2019-02-17 18:22:29 +0100

    terms sounds page filled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { QuestionnairePageComponent } from './pages/questionnaire-page/questionnaire-page.component'
 import { PollDescriptionPageComponent } from './pages/poll-description-page/poll-description-page.component';
+import { TermsSoundsPageComponent } from './pages/terms-sounds-page/terms-sounds-page.component';
 
 const routes: Routes = [
   { path: '', component: WelcomePageComponent },
   { path: 'questionnaire', component: QuestionnairePageComponent },
-  { path: 'poll-description', component: PollDescriptionPageComponent }
+  { path: 'poll-description', component: PollDescriptionPageComponent },
+  { path: 'terms-sounds', component: TermsSoundsPageComponent }
 ];
 
 @NgModule({
